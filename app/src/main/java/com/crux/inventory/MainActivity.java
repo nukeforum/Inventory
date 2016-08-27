fe395af35c3c7ef631844b82515ca1d7ee591cdd
Benjamin Zweber <benjamin.zweber@gpsinsight.com>
2016-08-27 15:46:28 -0700

    added butterknife and ids for list and fab

--- app/src/main/java/com/crux/inventory/MainActivity.java
+++ app/src/main/java/com/crux/inventory/MainActivity.java
@@ -1,11 +1,21 @@
 package com.crux.inventory;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 import com.gpsinsight.inventory.R;
 
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.item_list)
+    RecyclerView itemList;
+
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
