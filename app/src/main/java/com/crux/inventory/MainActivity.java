3d613e90edfc4b3d04c71d20bbbad1312c878f38
Nathan Reline <nathanielreline@gmail.com>
2016-08-27 15:30:38 -0700

    Recyclerview

--- app/src/main/java/com/crux/inventory/MainActivity.java
+++ app/src/main/java/com/crux/inventory/MainActivity.java
@@ -1,9 +1,9 @@
 package com.crux.inventory;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import io.realm.Realm;
+import com.gpsinsight.inventory.R;
 
 public class MainActivity extends AppCompatActivity {
 
