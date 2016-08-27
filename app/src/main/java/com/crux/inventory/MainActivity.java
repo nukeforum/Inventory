fb7129a503e93675543585e2f25e07eca09b0f2e
Nathan Reline <nathanielreline@gmail.com>
2016-08-27 16:44:03 -0700

    Dynamic adding to realm and view updates

--- app/src/main/java/com/crux/inventory/MainActivity.java
+++ app/src/main/java/com/crux/inventory/MainActivity.java
@@ -7,7 +7,9 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 
 import com.crux.inventory.recyclerview.ItemAdapter;
 import com.gpsinsight.inventory.R;
@@ -42,20 +44,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showDialog() {
+        final View layout = LayoutInflater.from(this).inflate(R.layout.new_item_dialog, null);
+        final EditText editText = (EditText) layout.findViewById(R.id.item_naming_field);
         new AlertDialog.Builder(this)
                 .setMessage("Add item")
                 .setPositiveButton("Add", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         Realm realm = Realm.getDefaultInstance();
-                        Item item = new Item("Mrs.Buttersworth");
+                        Item item = new Item(editText.getText().toString());
                         realm.beginTransaction();
                         realm.insertOrUpdate(item);
                         realm.commitTransaction();
                     }
                 })
                 .setNegativeButton("Cancel", null)
-                .setView(R.layout.new_item_dialog)
+                .setView(layout)
                 .create()
                 .show();
     }
